2d2cc6c386be416f7b2b9bd1e8977aac565b6922
caoyuanlang <752902963@qq.com>
2019-09-04 23:28:49 +0800

    把本地工程添加到git

---
+++ git-test1/demo-git-test1/src/cn/itcast/gitDemo1.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class gitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("gitDemo1.....");
+        System.out.println("gitDemo1.....");
+        System.out.println("gitDemo1.....");
+        System.out.println("gitDemo1.....");
+        System.out.println("gitDemo1.....");
+    }
+}
