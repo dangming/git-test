b18b5b3ac7c776b4875175d0ec8a1fb4a8cb7caa
caoyuanlang <752902963@qq.com>
2019-09-05 00:03:25 +0800

    修改test1

--- git-test1/demo-git-test1/src/cn/itcast/gitDemo1.java
+++ git-test1/demo-git-test1/src/cn/itcast/gitDemo1.java
@@ -7,11 +7,13 @@ public class gitDemo1 {
         System.out.println("gitDemo1.....");
         System.out.println("gitDemo1.....");
         System.out.println("gitDemo1.....");
+        //hello test1
     }
 
 
     public void test(){
         int i = 0;
         i+=10;
+        System.out.println(i);
     }
 }
